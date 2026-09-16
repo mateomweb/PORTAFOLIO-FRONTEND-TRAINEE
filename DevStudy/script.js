f7807ac6b04b9efeed2c8ae7d588d994a430b227
mateo moori <eliassayayinpro@gmail.com>
2026-09-16 05:12:58 -0300

    proyecto DevStudy con HTML CSS y  estudio de JavaScript

---
+++ DevStudy/script.js
@@ -0,0 +1,13 @@
+let boton  = document.getElementById("boton-explorar");
+
+boton.addEventListener("click", () => {
+
+    alert("hiciste click en este boton");
+});
+
+let boton2  = document.getElementById("boton-conocer");
+
+  boton2.addEventListener("click", () => {
+
+    alert("hiciste click en este boton");
+});
